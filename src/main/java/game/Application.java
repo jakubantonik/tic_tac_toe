8eb1c4d80ddeebb227ec6fe52715b506ff9d75bc
Jakub Antonik <https://github.com/jakubantonik>
2023-01-11 14:57:14 +0100

    game

--- src/main/java/game/Application.java
+++ src/main/java/game/Application.java
@@ -3,6 +3,8 @@ package game;
 import java.util.Scanner;
 
 public class Application {
+
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -11,33 +13,39 @@ public class Application {
         System.out.println("Witaj w grze");
         System.out.println("Wybierz wariant gry: \n1 - Gra z kompanem \n2 - Gra z komputerem");
         int version = scan.nextInt();
-        System.out.println("Wprowadź ilość wygranych");
-        char symbol = 'X';
+        System.out.println("Podaj do ilu wygranych ma toczyć się rozgrywka");
+        int numbersOfWins = scan.nextInt();
+        int wins =0;
 
-        while (true) {
+
+        char symbol = 'X';
+        boolean win = true;
+
+
+        do {
             if(version == 1) {
                 PlayerMove.getPlayerMove(board.array, symbol);
                 board.getBoard();
-                if (MoveUtil.checkResult(board.array, symbol)) {
-                    break;
+                if (MoveUtil.checkResult(board.array, symbol, wins, numbersOfWins)) {
+                    continue;
                 }
                 symbol = (symbol == 'X') ? 'O' : 'X';
             } else if (version == 2) {
                 PlayerMove.getPlayerMove(board.array, symbol);
                 board.getBoard();
-                if (MoveUtil.checkResult(board.array, symbol)) {
-                    break;
+                if (MoveUtil.checkResult(board.array, symbol, wins, numbersOfWins)) {
+                    continue;
                 }
                 System.out.println("Ruch komputera: ");
-                symbol = (symbol == 'X') ? 'O' : 'X';
+                symbol = 'O';
                 ComputerMove.getComputerMove(board.array, symbol);
                 board.getBoard();
-                if (MoveUtil.checkResult(board.array, symbol)) {
-                    break;
+                if (MoveUtil.checkResult(board.array, symbol, wins, numbersOfWins)) {
+                    continue;
                 }
-                symbol = (symbol == 'X') ? 'O' : 'X';
+                symbol = 'X';
             }
-        }
+        } while (numbersOfWins > wins);
     }
 }
 
--- src/main/java/game/MoveUtil.java
+++ src/main/java/game/MoveUtil.java
@@ -1,39 +1,50 @@
 package game;
 
 public class MoveUtil {
-    public static boolean checkingRows(char [][] array, char symbol){
+    static Board board = new Board();
+
+    private int wins = 0;
+    public static boolean checkingRows(char [][] array, char symbol, int wins, int numberOfWins){
         for(int n=0; n<3; n++){
             if (array[n][0] == symbol && array[n][1] == symbol && array[n][2] == symbol){
-                System.out.println("Gracz: " + symbol + " wygrywa!");
-                return true;
+                return newWin(symbol, wins, numberOfWins);
             }
         }
         return false;
     }
-    public static boolean checkingColumns(char [][] array, char symbol){
+
+    public static boolean checkingColumns(char [][] array, char symbol, int wins, int numberOfWins){
         for(int i=0; i<3; i++){
             if (array[0][i] == symbol && array[1][i] == symbol && array[2][i] == symbol){
-                System.out.println("Gracz: " + symbol + " wygrywa!");
-                return true;
+                return newWin(symbol, wins, numberOfWins);
             }
         }
         return false;
     }
-    public static boolean checkingSlants(char [][] array, char symbol){
+    public static boolean checkingSlants(char [][] array, char symbol, int wins, int numberOfWins){
         if (array[0][0] == symbol && array[1][1] == symbol && array[2][2] == symbol){
-            System.out.println("Gracz: " + symbol + " wygrywa!");
-            return true;
+            return newWin(symbol, wins, numberOfWins);
         }
         if (array[2][0] == symbol && array[1][1] == symbol && array[0][2] == symbol){
-            System.out.println("Gracz: " + symbol + " wygrywa!");
-            return true;
+            return newWin(symbol, wins, numberOfWins);
         }
         return false;
     }
-    public static boolean checkResult(char [][] array, char symbol){
-        boolean winRows = checkingRows(array, symbol);
-        boolean winColumns = checkingColumns(array, symbol);
-        boolean winSlants = checkingSlants(array, symbol);
+    private static boolean newWin (char symbol, int wins, int numberOfWins) {
+        wins++;
+        System.out.println("Ilość wygranych rund :" + wins);
+        if(wins < numberOfWins){
+            System.out.println("Rozpoczynamy kolejną rundę!");
+        } else {
+            System.out.println("Gracz: " + symbol + " wygrywa!");
+        }
+
+        return true;
+    }
+    public static boolean checkResult(char [][] array, char symbol, int wins, int numberOfWins){
+        boolean winRows = checkingRows(array, symbol, wins, numberOfWins);
+        boolean winColumns = checkingColumns(array, symbol, wins, numberOfWins);
+        boolean winSlants = checkingSlants(array, symbol, wins, numberOfWins);
         return winRows || winColumns || winSlants;
     }
 }
